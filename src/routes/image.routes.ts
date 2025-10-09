df35b9075851f77027d07840a097e24c1c3f5798
Kaif Ayaz Khan <kaifayazkhan@gmail.com>
2025-10-09 14:02:23 +0530

    Add rate limiting and standardize API responses (#5)
    * feat: add rate limit on upload and transform endpoints
    
    * feat: implement ApiResponse class for standardized API responses

--- src/routes/image.routes.ts
+++ src/routes/image.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 import {
   uploadImage,
@@ -9,9 +10,27 @@ import {
 
 const router: Router = Router();
 
+const uploadLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 10,
+  message: 'Too many requests, please try again after 5 minutes',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const transformLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 20,
+  message: 'Too many requests, please try again after 5 minutes',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 router.route('/').get(verifyJWT, getUserTransformedImages);
 router.route('/:id').get(verifyJWT, getTransformedImageById);
-router.route('/upload').post(verifyJWT, uploadImage);
-router.route('/:id/transform').post(verifyJWT, transformImage);
+router.route('/upload').post(verifyJWT, uploadLimiter, uploadImage);
+router
+  .route('/:id/transform')
+  .post(verifyJWT, transformLimiter, transformImage);
 
 export default router;
